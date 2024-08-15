e89361fe6bf59f267f0951cc9a67b449441d79b4
leeangela9 <angelaaa0927@gmail.com>
2024-08-15 14:32:43 -0400

    changed front-end

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let imgView = document.getElementById("img-view");
 let source = document.getElementById("vid");
 let dropTxt = document.getElementById("drop-txt");
 let form = document.getElementById("form");
+let vidContainer = document.getElementById("v-container");
+let loader = document.getElementById("loading-box");
 
 let startMin = document.getElementById("start-min");
 let startSec = document.getElementById("start-sec");
@@ -19,98 +21,124 @@ let submitBtn = document.getElementById("submit");
 inputFile.addEventListener("change", uploadVid);
 
 function uploadVid() {
-    let vidLink = URL.createObjectURL(inputFile.files[0]);
-    source.src = vidLink;
-    source.style.width = '90%';
-    dropTxt.textContent = "";  
+  let vidLink = URL.createObjectURL(inputFile.files[0]);
+  source.src = vidLink;
+  source.style.display = "block";
+
+  source.addEventListener("loadedmetadata", function () {
+    const width = source.videoWidth;
+    const height = source.videoHeight;
+    const ratio = width / height;
+    const max = 450;
+    let containerWidth, containerHeight;
+
+    if (ratio > 1) {
+      containerWidth = max;
+      containerHeight = max / ratio;
+      console.log(width, height, ratio, "->", containerWidth, containerHeight);
+    } else {
+      containerWidth = max * ratio;
+      containerHeight = max;
+      console.log(width, height, ratio, "->", containerWidth, containerHeight);
+    }
+
+    vidContainer.style.width = containerWidth + "px";
+    vidContainer.style.height = containerHeight + "px";
+    dropTxt.textContent = "";
+  });
 }
 
 function handleFile() {
-    // e.preventDefault();
+  // e.preventDefault();
 
-    const file = inputFile.files[0];
+  const file = inputFile.files[0];
 
-    if (file) {
+  if (file) {
+    source.src = "";
+    timeDiv.style.display = "none";
+    soundDiv.style.display = "none";
+    submitBtn.style.display = "none";
+    source.style.display = "hidden";
+    loader.style.display = "block";
 
-        let clippedLink = "";
+    let clippedLink = "";
 
-        let audioBool = "M";
-        if (audio.checked) {
-            audioBool = "A";
-        }
-
-        const duration = Math.round(source.duration);
-
-        let endingSec = 0;
-        const endingInput = Number(endMin.value) * 60 + Number(endSec.value);
-
-        if ((endMin.value !== "" || endSec.value !== "") && (endingInput <= duration))
-            endingSec = endingInput;
-        else 
-            endingSec = duration;
-
-        let startingSec = 0;
-        const startingInput = Number(startMin.value) * 60 + Number(startSec.value);
-        if (startingInput < endingSec)
-            startingSec = startingInput;
-
-        const newFileName = `${file.name.split(".")[0]}${startingSec}${endingSec}${audioBool}`;
-        console.log(newFileName);
-
-        const apiUrl = "https://4f1wirqn9k.execute-api.us-east-1.amazonaws.com/Prod/upload";
-
-        source.src = "loading.mp4";
-        timeDiv.style.display = "none";
-        soundDiv.style.display = "none";
-        submitBtn.style.display = "none";
-
-        axios.post(apiUrl, {
-            filename: newFileName + ".mp4",
-            withCredentials: false,
-
-        })
-        .then(function(response) {
-            console.log("api response: ", response);
-
-            const uploadUrl = response.data.uploadUrl;
-            const fileKey = response.data.fileKey;
-
-            console.log(uploadUrl);
-            console.log(fileKey);
-
-            const postUrl = "https://941jhpc24m.execute-api.us-east-1.amazonaws.com/Prod/clip";
-            
-            axios.put(uploadUrl, file, {
-                headers: {
-                    "Content-Type": 'video/mp4'
-                }, 
-            }) 
-            .then(function(response) {
-
-                console.log("Success: ", response);
-
-                axios.post(postUrl, {
-                    filename: fileKey,
-                })
-                .then(function(response) {
-                    console.log("Response: ", response);
-                    clippedLink = response.data.url;
-                    console.log("Clipped Link: ", clippedLink);
-                    source.src = "done.mp4";
-                    dDiv.style.display = "block";
-                    dBtn.href = clippedLink;
-                })
-                .catch(function(error) {
-                    console.log("error 1: ", error);
-                })
-            })
-            .catch(function(error) {
-                console.log("error 2: ", error);
-            })
-        })
-        .catch(function(error) {
-            console.log("error 3: ", error);
-        });  
+    let audioBool = "M";
+    if (audio.checked) {
+      audioBool = "A";
     }
 
+    const duration = Math.round(source.duration);
+
+    let endingSec = 0;
+    const endingInput = Number(endMin.value) * 60 + Number(endSec.value);
+
+    if ((endMin.value !== "" || endSec.value !== "") && endingInput <= duration)
+      endingSec = endingInput;
+    else endingSec = duration;
+
+    let startingSec = 0;
+    const startingInput = Number(startMin.value) * 60 + Number(startSec.value);
+    if (startingInput < endingSec) startingSec = startingInput;
+
+    const newFileName = `${
+      file.name.split(".")[0]
+    }${startingSec}${endingSec}${audioBool}`;
+    console.log(newFileName);
+
+    const apiUrl =
+      "https://4f1wirqn9k.execute-api.us-east-1.amazonaws.com/Prod/upload";
+
+    axios
+      .post(apiUrl, {
+        filename: newFileName + ".mp4",
+        withCredentials: false,
+      })
+      .then(function (response) {
+        console.log("api response: ", response);
+
+        const uploadUrl = response.data.uploadUrl;
+        const fileKey = response.data.fileKey;
+
+        console.log(uploadUrl);
+        console.log(fileKey);
+
+        const postUrl =
+          "https://941jhpc24m.execute-api.us-east-1.amazonaws.com/Prod/clip";
+
+        axios
+          .put(uploadUrl, file, {
+            headers: {
+              "Content-Type": "video/mp4",
+            },
+          })
+          .then(function (response) {
+            console.log("Success: ", response);
+
+            axios
+              .post(postUrl, {
+                filename: fileKey,
+              })
+              .then(function (response) {
+                console.log("Response: ", response);
+                clippedLink = response.data.url;
+                console.log("Clipped Link: ", clippedLink);
+                loader.style.display = "hidden";
+                source.style.display = "block";
+                source.src = "done.mp4";
+                dDiv.style.display = "block";
+                dBtn.href = clippedLink;
+              })
+              .catch(function (error) {
+                console.log("error 1: ", error);
+              });
+          })
+          .catch(function (error) {
+            console.log("error 2: ", error);
+          });
+      })
+      .catch(function (error) {
+        console.log("error 3: ", error);
+      });
+  }
 }
